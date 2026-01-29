ae995ff583460c0f91184de1ec6fed5c7359aca1
Isaries <leolove3very@gmail.com>
2026-01-29 11:20:06 +0800

    feat: add PDF_EXPORT_MAX_WORKERS config

--- backend/config.py
+++ backend/config.py
@@ -15,7 +15,8 @@ load_dotenv(env_path)
 # File Paths
 TEMP_PDF_IMG_DIR = os.path.join(basedir, 'temp_pdf_images')
 TEMP_PDF_EXPORT_DIR = os.path.join(basedir, 'temp_pdf_exports')
-PDF_EXPORT_TTL_HOURS = 24
+PDF_EXPORT_TTL_HOURS = 24  # Auto-cleanup after 24 hours
+PDF_EXPORT_MAX_WORKERS = int(os.getenv('PDF_EXPORT_MAX_WORKERS', '2'))  # Parallel workers
 GROUPS_FILE = 'groups.json'
 
 USERS_FILE = 'users.json'
